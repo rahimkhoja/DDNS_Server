511571fad3bb57abb746238f2e23a05fad95bf7f
Rahim Khoja <CanadianRepublican@users.noreply.github.com>
2017-04-16 12:48:08 -0700

    Create ddns-cron.sh

---
+++ utils/var/ddns/ddns-cron.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# DDNS Cron Script
+# By Rahim Khoja for DDNS Server
+
+d=$(date +%Y-%m-%d)
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+
+# Database Properties
+user="dyn_cron_user"
+dbname="dyn_server_db"
+password="password"
+
+# Update Accounts that have Forgot Status for more than 1 Day.
+mysql -u $user -D $dbname -p$password -e "UPDATE dyn_server_db.dyn_users SET forgot=0 WHERE forgot=1 AND forgot_date <= (now() - INTERVAL 1 DAY);"
+
+# Delete Accounts that have not been enabled within 1 Day.
+mysql -u $user -D $dbname -p$password -e "DELETE FROM dyn_server_db.dyn_users WHERE ( enable_date IS null OR enable_date='0000-0-0' ) AND enabled=0 AND create_date <= (now() - INTERVAL 1 DAY);"
+
+echo "DDNS Cron Script Ran On $dt " >> /var/ddns/log/ddns-$d.log
