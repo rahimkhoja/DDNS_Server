1adaa37f9f933d7dee34513d37ee00a5c2b9238c
Rahim Khoja <CanadianRepublican@users.noreply.github.com>
2017-06-26 04:07:51 -0700

    Update ddns_slave_deployment.sh

--- ddns_slave_deployment.sh
+++ ddns_slave_deployment.sh
@@ -3,6 +3,86 @@
 # for CentOS 7 
 # By Rahim Khoja (rahimk@khojacorp.com)
 
+# Default Properties
+defaultslave=2
+defaultmasterhost=172.16.254.40
+defaultmasterpass=password
+defaultmysqlpass=password
+defaultnamedpass=password
+
+# Get Slave Server Number
+finish="-1"
+while [ "$finish" = '-1' ]
+  do
+    finish="1"
+    read -p "Please Enter Slave Server Number [$defaultslave]: " SLAVENUM
+    SLAVENUM=${SLAVENUM:-$defaultslave}
+    echo
+    read -p "Slave Number is: $SLAVENUM [y/n]? " answer
+
+    if [ "$answer" = '' ];
+    then
+      answer=""
+    else
+      case $answer in
+        y | Y | yes | YES ) answer="y";;
+        n | N | no | NO ) answer="n"; finish="-1";;
+        *) finish="-1";
+           echo -n 'Invalid Response\n';
+       esac
+    fi
+done
+echo
+
+# Get Master Server Host
+finish="-1"
+while [ "$finish" = '-1' ]
+  do
+    finish="1"
+    read -p "Please Enter Master MySQL Server IP or Hostname [$defaultmasterhost]: " MASTERHOST
+    MASTERHOST=${MASTERHOST:-$defaultmasterhost}
+    echo
+    read -p "Master Server Host is: $MASTERHOST [y/n]? " answer
+
+    if [ "$answer" = '' ];
+    then
+      answer=""
+    else
+      case $answer in
+        y | Y | yes | YES ) answer="y";;
+        n | N | no | NO ) answer="n"; finish="-1";;
+        *) finish="-1";
+           echo -n 'Invalid Response\n';
+       esac
+    fi
+done
+echo
+
+# Get Master MySQL Replication Server Password
+finish="-1"
+while [ "$finish" = '-1' ]
+  do
+    finish="1"
+    read -p "Please Enter Master MySQL Server Replication Password [$defaultmasterpass]: " MASTERPASS
+    MASTERPASS=${MASTERPASS:-$defaultmasterpass}
+    echo
+    read -p "Master Server MySQL Replication Password is: $MASTERPASS [y/n]? " answer
+
+    if [ "$answer" = '' ];
+    then
+      answer=""
+    else
+      case $answer in
+        y | Y | yes | YES ) answer="y";;
+        n | N | no | NO ) answer="n"; finish="-1";;
+        *) finish="-1";
+           echo -n 'Invalid Response\n';
+       esac
+    fi
+done
+echo
+
+
 # CentOS 7 System Update & Dependency Installation
 yum -y update && yum -y upgrade
 yum -y groupinstall 'Development Tools'
@@ -62,3 +142,15 @@ mysql -u root -ppassword dyn_server_db < /tmp/DDNS/dyn_server_db.sql
 
 # Setup Slave Databases
 mysql -u root -ppassword -e "CHANGE MASTER TO MASTER_HOST='172.16.254.40', MASTER_USER='dyn_replicator', MASTER_PASSWORD='password'; GRANT SELECT,LOCK TABLES on dyn_server_db.* to 'dyn_dns_user'@'localhost' identified by 'password'; flush privileges;"
+
+# Firewall Configuration
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --zone=public --permanent --add-port=53/udp
+sudo firewall-cmd --zone=public --permanent --add-port=53/tcp
+sudo firewall-cmd --reload
+
+# Start Named with DLZ Support
+systemctl daemon-reload
+systemctl enable named
+systemctl start named
