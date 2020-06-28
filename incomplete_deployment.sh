668ca1dd85529ab48cdf27f740ce19830202c235
Rahim Khoja <rahim@khoja.ca>
2020-06-27 17:52:15 -0700

    Update incomplete_deployment.sh

--- incomplete_deployment.sh
+++ incomplete_deployment.sh
@@ -218,7 +218,10 @@ fi
 
 
 
-
+# Master Server Prompts
+# -> Automatically Generate Random Password
+# -> (If No) -> Enter Custom Password
+# ->         -> -> Confirm Custom Password
 
 
 
@@ -283,18 +286,64 @@ fi
 
 
 
+# Install Bind/Named with DLZ Support
+# Version 9-11-0-p3 from www.isc.org
+curl -o /tmp/bind-9.11.0-P3.tar.gz https://www.isc.org/downloads/file/bind-9-11-0-p3/
+cd /tmp
+tar -xvzf bind-9.11.0-P3.tar.gz
+mv /tmp/bind-9.11.0-P3 /usr/src/
+cd /usr/src/bind-9.11.0-P3/
+./configure --prefix=/usr --sysconfdir=/etc/bind --localstatedir=/var --mandir=/usr/share/man --infodir=/usr/share/info --enable-threads --enable-largefile --with-libtool --enable-shared --enable-static --with-openssl=/usr --with-gssapi=/usr --with-gnu-ld --with-dlz-postgres=no --with-dlz-mysql=yes --with-dlz-bdb=no --with-dlz-filesystem=yes --with-dlz-ldap=no --with-dlz-stub=yes --enable-ipv6 LDFLAGS="-I/usr/include/mysql -L/usr/lib64/mysql"
+make; make install
+ldconfig -v
 
+# Named User and Group Setup
+groupadd named
+useradd -d /var/named -g named -s /bin/false named
+mkdir -p /var/named/
+chown -R named:named /var/named
+chmod -R 755 /var/named
+mkdir /var/log/named
+chown named:named /var/log/named
+mkdir /var/run/named
+chown named:named /var/run/named
+chown named:named /usr/sbin/named
 
+# Selinux Named Server Commands 
+semanage permissive -a named_t
+setsebool -P named_write_master_zones=1
 
+# Named SystemD Service File
+cat <<EOF >>/usr/lib/systemd/system/named.service
+[Unit]
+Description=Berkeley Internet Name Domain (DNS) with DLZ
+After=network.target
 
+[Service]
+Type=simple
+Restart=always
+RestartSec=60
+ExecStart=/usr/sbin/named -u named -c /etc/named.conf -f -d 9
 
+[Install]
+WantedBy=multi-user.target
+EOF
 
+# Reload SystemD Services List & Enabled Named Service on Startup
+systemctl daemon-reload
+systemctl enable named
 
+# Tomcat 9 Install
+yum -y install java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel.x86_64
+cd /tmp
+wget http://apache.mirror.globo.tech/tomcat/tomcat-9/v9.0.0.M21/bin/apache-tomcat-9.0.0.M21.tar.gz
+tar -xzvf apache-tomcat-9.0.0.M21.tar.gz
+mv apache-tomcat-9.0.0.M21 /opt/tomcat
 
-
-
-
-
+# Tomcat User and Group Setup
+groupadd tomcat 
+useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+chown -hR tomcat:tomcat /opt/tomcat
 
 
 
@@ -306,17 +355,7 @@ fi
 cd /tmp
 git clone https://github.com/CanadianRepublican/DDNS_Server.git DDNS
 
-# Bind User & Directory Creation
-groupadd named
-useradd -d /var/named -g named -s /bin/false named
-mkdir -p /var/named/
-chown -R named:named /var/named
-chmod -R 755 /var/named
-mkdir /var/log/named
-chown named:named /var/log/named
-mkdir /var/run/named
-chown named:named /var/run/named
-chown named:named /usr/sbin/named
+
 
 # Master Database Setup
 
@@ -332,8 +371,7 @@ make
 make install
 ldconfig -v
 
-semanage permissive -a named_t
-setsebool -P named_write_master_zones=1
+
 
 groupadd ddns
 useradd -d /var/ddns -g ddns -s /bin/false ddns
@@ -345,23 +383,7 @@ chmod -R 755 /var/ddns
 crontab -l -u ddns | echo '*/5 * * * * /var/ddns/ddns-cron.sh' | crontab -u ddns -
 
 
-vi /usr/lib/systemd/system/named.service
 
-Copy Below Here 
-[Unit]
-Description=Berkeley Internet Name Domain (DNS) with DLZ
-After=network.target
-
-[Service]
-Type=simple
-Restart=always
-RestartSec=60
-ExecStart=/usr/sbin/named -u named -c /etc/named.conf -f -d 9
-
-[Install]
-WantedBy=multi-user.target
-
-Copy Above Here
 
 systemctl daemon-reload
 
@@ -401,6 +423,11 @@ mysql -u root -p dyn_server_db < dyn_server_db.sql
 sudo systemctl restart mariadb 
 
 
+systemctl daemon-reload
+
+# Database Setup
+systemctl enable mariadb
+
 cat <<EOF >>/etc/my.cnf.master
 bind-address=0.0.0.0
 server-id=1
@@ -430,16 +457,7 @@ grant all on dyn_server_db.* to 'dyn_web_app'@'localhost' identified by 'Passwor
 
 
 
-// Tomcat 9 Install
-yum -y install java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel.x86_64
-groupadd tomcat 
-useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
-cd /tmp
-wget http://apache.mirror.globo.tech/tomcat/tomcat-9/v9.0.0.M21/bin/apache-tomcat-9.0.0.M21.tar.gz
-tar -xzvf apache-tomcat-9.0.0.M21.tar.gz
-mv apache-tomcat-9.0.0.M21 /opt/tomcat
-cd /opt
-chown -hR tomcat:tomcat tomcat
+
 
 
 # Setup FirewallD
